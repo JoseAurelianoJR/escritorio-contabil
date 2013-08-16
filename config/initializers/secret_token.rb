9cf53709f981bccb9ed5771c61ee34e5235e5384
JoseAurelianoJR <junior.rjsistemas@gmail.com>
2013-08-16 13:34:44 -0300

    Commit Inicial dos Fontes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EscritorioContabil::Application.config.secret_token = 'e16f21c57d88a257542e78fbee48373caade321a1eee54cee005caa30c906adca91f818038b0ec8bac88f8eb8ecbd9f84b8e1350b60471a9a87bc00aa6dd80a0'
